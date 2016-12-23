145b0aeaf651693bbb6bebaca616fd9664d2c24f
Selcher <selcher123@gmail.com>
2016-12-23 19:32:46 +0800

    Update bot to use intents

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ var builder = require('botbuilder');
 var server = restify.createServer();
 
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-	console.log('%s listening to %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-	appId: process.env.MICROSOFT_APP_ID,
-	appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
+var intents = new builder.IntentDialog();
 
 server.post('/api/messages', connector.listen());
 
@@ -25,6 +26,40 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //==============
 
-bot.dialog('/', function (session) {
-	session.send('Hello World');
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onBegin(function (session, args, next) {
+    next();
 });
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
